10afc4a34d286e72d12f4489aadfe346901ef554
Soham <156944394+sohampatra82@users.noreply.github.com>
2026-07-25 12:24:25 +0530

    initialize pwa

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "hrms-v1";
+
+const urlsToCache = ["/", "/css/style.css", "/js/main.js", "/icons/logo.png", "/manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
